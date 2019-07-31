ca6416dd1227259d1ce8636cb61680c962b9bf5b
Yago Lopes <yagolopeslazaro@gmail.com>
2019-07-31 15:58:44 -0300

    organizando arquivos de instalação

--- install-my-apps.sh
+++ install-my-apps.sh
@@ -18,54 +18,17 @@ if ! apt-get dist-upgrade -y; then
 fi
 echo "Atualização de pacotes feita com sucesso"
 
-# Instalando os pacotes que já estão nos repositórios oficias do sistema
+# Instalando os pacotes atravez do gerenciador apt
 
-if ! apt-get install gimp -y; then
-    echo "Não foi possível instalar o pacote GIMP"
+if ! apt install git docker docker-compose nodejs npm software-properties-common apt-transport-https wget gimp openjdk-8-jre-headless filezilla snapd samba brasero libgconf-2-4 libappindicator1 -y; then
+    echo "Não foi possível instalar os pacotes travez do gerenciador apt-get"
     exit 1
 fi
-if ! apt-get install openjdk-8-jre-headless -y; then
-    echo "Não foi possível instalar o pacote filezilla"
-    exit 1
-fi
-if ! apt-get install filezilla -y; then
-    echo "Não foi possível instalar o pacote filezilla"
-    exit 1
-fi
-if ! apt-get install snapd -y; then
-    echo "Não foi possível instalar o pacote snapd"
-    exit 1
-fi
-if ! apt-get install samba -y; then
-    echo "Não foi possível instalar o pacote samba"
-    exit 1
-fi
-if ! apt-get install brasero -y; then
-    echo "Não foi possível instalar o pacote brasero"
-    exit 1
-fi
-#if ! snap install sudo snap install sublime-text-3; then
-#   echo "Não foi possível instalar o pacote sublime-text-3"
-#    exit 1
-#fi
-if ! snap install discord; then
-    echo "Não foi possível instalar o pacote discord"
-    exit 1
-fi
-if ! snap install figma-linux; then
-    echo "Não foi possível instalar o pacote figma-linux"
-    exit 1
-fi
-if ! apt install libgconf-2-4 libappindicator1 -y; then
-    echo "Não foi possível instalar o pacote libgconf-2-4 libappindicator1"
-    exit 1
-fi
-if ! snap install spotify; then
-    echo "Não foi possível instalar o pacote discord"
-    exit 1
-fi
-if ! snap install insomnia; then
-    echo "Não foi possível instalar o pacote discord"
+
+# Instalando os pacotes atravez do gerenciador snap
+
+if ! snap install discord figma-linux spotify insomnia; then
+    echo "Não foi possível instalar os pacotes travez do gerenciador apt-get"
     exit 1
 fi
 
@@ -73,31 +36,27 @@ fi
 
 # Instalando os pacotes que não estão nos repositórios oficias do sistema
 
-# Instalando kadenlive
+# Adicionando chaves de repo e novos repositórios
 echo Adicionando novo repositório..
-if ! add-apt-repository ppa:kdenlive/kdenlive-stable && apt-get update && apt-get install kdenlive -y; then
-    echo "Houve um problema coma a instalação do kadenlive atravéz do ppa:kdenlive/kdenlive-stable"
+
+if !wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - && add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" && add-apt-repository ppa:kdenlive/kdenlive-stable && add-apt-repository ppa:obsproject/obs-studio && wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - && add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"; then
+    echo "Erro ao adicionar repositórios. Verifique se nenhum dos repósitorios estão quebrados ou se existe alguma chave invalida invalidas."
     exit 1
 fi
 
-# Install ffmpeg
-if ! apt-get install ffmpeg -y; then
-    echo "Não foi possível instalar o pacote ffmpeg"
-    exit 1
-fi
-
-# Instalando obs-studio
-if ! add-apt-repository ppa:obsproject/obs-studio && apt-get update && apt-get install obs-studio -y; then
+# Atualizando nova lista de repos
+if !apt-get update; then
     echo "Houve um problema coma a instalação do obs-studio atravéz do ppa:obsproject/obs-studio"
     exit 1
 fi
 
-#virtual box
-#vs code
-#node
-#npm
-#yarn
-#react
-#docker
-#docker-compose
-#git
+# Instalando novos apps
+if ! apt-get install code ffmpeg obs-studio sublime-text kdenlive -y; then
+    echo "Não foi possivel instalar os novos pacotes atrevez do gerenciador apt-get"
+    exit 1
+fi
+
+if !npm i yarn create-react-app -g; then
+    echo "Não foi possive instalar pacotes via npm"
+    exit 1
+fi
