a479111140df475b644228f929e04e9b65a36b7d
Yago Lopes <yagolopeslazaro@gmail.com>
2019-08-01 17:15:27 -0300

    add rar, yarn e netbeans

--- install-my-apps.sh
+++ install-my-apps.sh
@@ -20,7 +20,7 @@ echo "Atualização de pacotes feita com sucesso"
 
 # Instalando os pacotes atravez do gerenciador apt
 
-if ! apt install git docker docker-compose nodejs npm software-properties-common apt-transport-https wget gimp openjdk-8-jre-headless filezilla snapd samba brasero libgconf-2-4 libappindicator1 -y; then
+if ! apt install git docker docker-compose nodejs npm rar yarn software-properties-common apt-transport-https wget gimp openjdk-8-jre-headless filezilla snapd samba brasero libgconf-2-4 libappindicator1 netbeans -y; then
     echo "Não foi possível instalar os pacotes travez do gerenciador apt-get"
     exit 1
 fi
@@ -39,24 +39,24 @@ fi
 # Adicionando chaves de repo e novos repositórios
 echo Adicionando novo repositório..
 
-if !wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - && add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" && add-apt-repository ppa:kdenlive/kdenlive-stable && add-apt-repository ppa:obsproject/obs-studio && wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - && add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"; then
+if !add-apt-repository ppa:obsproject/obs-studio; then
     echo "Erro ao adicionar repositórios. Verifique se nenhum dos repósitorios estão quebrados ou se existe alguma chave invalida invalidas."
     exit 1
 fi
 
 # Atualizando nova lista de repos
-if !apt-get update; then
+if !apt update; then
     echo "Houve um problema coma a instalação do obs-studio atravéz do ppa:obsproject/obs-studio"
     exit 1
 fi
 
 # Instalando novos apps
-if ! apt-get install code ffmpeg obs-studio sublime-text kdenlive -y; then
+if ! apt install ffmpeg obs-studio -y; then
     echo "Não foi possivel instalar os novos pacotes atrevez do gerenciador apt-get"
     exit 1
 fi
 
-if !npm i yarn create-react-app -g; then
+if !npm i create-react-app -g; then
     echo "Não foi possive instalar pacotes via npm"
     exit 1
 fi
@@ -70,4 +70,7 @@ fi
 #Instalações à serem implementadas
 
 #React Native
+#code
+#sublime-text
+#kdenlive
 #Customização de barra de tarefa, tema, papel de parede etc...
